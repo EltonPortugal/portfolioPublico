69aeada53106efb174f94790c9512c131edb6401
EltonPortugal <eltonportugal@gmail.com>
2026-08-16 15:56:50 -0300

    Criação de variáveis

--- SistemaBancario/obj/Debug/net9.0/SistemaBancario.AssemblyInfo.cs
+++ SistemaBancario/obj/Debug/net9.0/SistemaBancario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b5598104d3673aa258ab8d043042a825ac96c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaBancario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
